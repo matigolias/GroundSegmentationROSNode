e66fc2a6f95fe69398fd96c454837bbd46c84981
Diogo Matias <diogomatias7@gmail.com>
2023-01-01 22:26:24 +0000

    test

--- src/alfa_node.cpp
+++ src/alfa_node.cpp
@@ -148,14 +148,16 @@ Mat AlfaNode::read_hardware_pointcloud(u64 *pointer, uint rows, uint cols)
     Mat hw_RI = Mat::zeros(64, 1800, CV_16UC1);
 
     uint size = rows * cols;
-    uint ddrSize = size/4; // since each position has 16 bits, 16*4=64 bit blocks
+    uint ddrSize = size/8; // since each position has 16 bits, 16*4=64 bit blocks
     uint8_t row = 0;
     uint16_t col = 0;
     // unsigned char* data = new unsigned char[size];
     // unsigned char* dataPtr = data;
-    for (uint i=0; i<ddrSize; i++) {
+    for (uint i=0; i<ddrSize*2; i++) {
         uint16_t a16_points[4];
+        cout << "Teste 1" << endl;
         memcpy((void*)(a16_points), pointer+i, sizeof(uint16_t)*4);
+        cout << "Teste 2" << endl;
         for(uint j=0; j<4; j++){
             if(row>=64)
             {
